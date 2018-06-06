2f61b334cf2da2c2f08c7474eb44eb62ef64866c
dlrlgugu <hckguja@gmail.com>
2018-06-06 14:33:39 -0700

    added max pooling Signed-off-by: dlrlgugu <hckguja@gmail.com>

--- cnn.py
+++ cnn.py
@@ -222,10 +222,16 @@ print("db_mean =", np.mean(db))
 
 
 
+def create_mask_from_window(x):
+    mask = x == np.max(x)
+    return mask
 
 
-
-
+np.random.seed(1)
+x = np.random.randn(2,3)
+mask = create_mask_from_window(x)
+print('x = ', x)
+print("mask = ", mask)
 
 
 
