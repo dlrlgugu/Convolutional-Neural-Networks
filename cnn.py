911aeda07594f48f13178ccc9183b4f531904126
dlrlgugu <hckguja@gmail.com>
2018-06-06 14:02:31 -0700

    z_mean value is wrong. Signed-off-by: dlrlgugu <hckguja@gmail.com>

--- cnn.py
+++ cnn.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 def zero_pad(X,pad):
     X_pad = np.pad(X,((0,0),(pad,pad),(pad,pad),(0,0)),'constant',constant_values=0)
     return X_pad
-
+#np.pad..
+"""
 np.random.seed(1)
 x = np.random.randn(4, 3, 3, 2)
 x_pad = zero_pad(x, 2)
@@ -21,3 +22,99 @@ axarr[0].imshow(x[0,:,:,0])
 axarr[1].set_title('x_pad')
 axarr[1].imshow(x_pad[0,:,:,0])
 plt.show()
+"""
+
+def conv_single_step(a_slice_prev, W, b):
+    s=np.multiply(a_slice_prev,W)
+    Z=np.sum(s)
+    Z=Z+float(b)
+    return Z
+"""
+np.random.seed(1)
+a_slice_prev = np.random.randn(4, 4, 3)
+W = np.random.randn(4, 4, 3)
+b = np.random.randn(1, 1, 1)
+
+Z = conv_single_step(a_slice_prev, W, b)
+print("Z =", Z)
+"""
+
+def conv_forward(A_prev, W, b, hparameters):
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    (f, f, n_C_prev, n_C) = W.shape
+    
+    stride = hparameters["stride"]
+    pad = hparameters["pad"]
+
+    n_H = int((n_H_prev-f+2*pad)/stride)+1
+    n_W = int((n_W_prev-f+2*pad)/stride)+1
+
+    Z = np.zeros((m,n_H,n_W,n_C))
+
+    A_prev_pad = zero_pad(A_prev, pad)
+
+    for i in range(m):        # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]    # Select ith training example's padded activation
+        #print(a_prev_pad.shape)#(8, 8, 3)
+        #print(a_prev_pad)
+        
+        for h in range(n_H_prev):      # loop over vertical axis of the output volume
+            for w in range(n_W_prev):  # loop over horizontal axis of the output volume
+                for c in range(n_C_prev):  # loop over channels (= #filters) of the output volume
+                    
+                    # Find the corners of the current "slice" (≈4 lines)
+                    #a_prev_pad
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
+                    
+                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                    a_slice_prev = a_prev_pad[vert_start:vert_end,horiz_start:horiz_end,:]
+                    
+                    # Convolve the (3D) slice with the correct filter W and bias b,
+                    # to get back one output neuron. (≈1 line)
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev, W[...,c], b[...,c])
+                                     
+    
+    # Making sure your output shape is correct
+    assert(Z.shape == (m, n_H, n_W, n_C))
+    
+    # Save information in "cache" for the backprop
+    cache = (A_prev, W, b, hparameters)
+    
+    return Z, cache
+    
+
+
+np.random.seed(1)
+A_prev = np.random.randn(10,4,4,3)
+W = np.random.randn(2,2,3,8)
+b = np.random.randn(1,1,1,8)
+hparameters = {"pad" : 2,
+               "stride": 2}
+
+Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+print("Z's mean =", np.mean(Z))
+print("Z[3,2,1] =", Z[3,2,1])
+print("cache_conv[0][1][2][3] =", cache_conv[0][1][2][3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
