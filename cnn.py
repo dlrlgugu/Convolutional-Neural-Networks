9390ff5f22be09602a8c580829d513cf4b4e0904
dlrlgugu <hckguja@gmail.com>
2018-06-06 13:07:13 -0700

    np.pad.. zero pad Signed-off-by: dlrlgugu <hckguja@gmail.com>

---
+++ cnn.py
@@ -0,0 +1,23 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+#To prevent shrink
+def zero_pad(X,pad):
+    X_pad = np.pad(X,((0,0),(pad,pad),(pad,pad),(0,0)),'constant',constant_values=0)
+    return X_pad
+
+np.random.seed(1)
+x = np.random.randn(4, 3, 3, 2)
+x_pad = zero_pad(x, 2)
+print ("x.shape =", x.shape)
+print ("x_pad.shape =", x_pad.shape)
+print ("x[1,1] =", x[1,1])
+print ("x_pad[1,1] =", x_pad[1,1])
+
+fig, axarr = plt.subplots(1, 2)
+axarr[0].set_title('x')
+axarr[0].imshow(x[0,:,:,0])
+axarr[1].set_title('x_pad')
+axarr[1].imshow(x_pad[0,:,:,0])
+plt.show()
